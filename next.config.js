ad3c9f0ad4d52822329f51a33b03294181c47332
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-11-06 01:41:43 +0900

    added sitemap generator

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const nextBuildId = require('next-build-id');
 module.exports = {
   generateBuildId: () => nextBuildId({ dir: __dirname }),
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/unknown': { page: '/unknown' },
+      '/stylish': { page: '/stylish' },
+    };
+  },
 };
