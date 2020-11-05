ccfc5a522ca152b23385fe9afd68edbfd27670b9
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-11-05 22:48:22 +0900

    fixed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-const isProd = (process.env.NODE_ENV || 'production') === 'production';
-
 module.exports = {
   exportPathMap: () => ({
     '/': { page: '/' },
   }),
-  assetPrefix: isProd ? '/code-hex.github.io' : '',
 };
