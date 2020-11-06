54ae3abfd63300961b2e886ace3c353f29c04619
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-11-06 18:11:53 +0900

    fixed home layout

--- pages/index.js
+++ pages/index.js
@@ -7,82 +7,93 @@ export default function Home() {
       <div className="marquee">
         <h1 className="title">Welcome to codehex's homepage</h1>
       </div>
-      <img alt="codehex" src="/assets/svg/codehex.svg" />
-      <p>My accounts</p>
-      <ul>
-        <li>
-          <span>
-            <a href="https://okinawa.pm.org/">Okinawa.pm</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            GitHub: <a href="https://github.com/Code-Hex/">Code-Hex</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            Twitter: <a href="https://twitter.com/codehex/">@codehex</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            Hatena: <a href="http://profile.hatena.ne.jp/codehex/">codehex</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            CPAN: <a href="https://metacpan.org/author/CODEHEX/">CODEHEX</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            Blog: <a href="https://codehex.hateblo.jp/">codehex.hateblo.jp</a>
-          </span>
-        </li>
-      </ul>
-      <p>Maps</p>
-      <ul className="page-list">
-        <li>
-          <Link href="/unknown" as="/unknown">
-            <a>My Unknown List</a>
-          </Link>
-        </li>
-        <li>
-          <a href="/wikipedia.html">Random Wikipedia</a>
-        </li>
-        <li>
-          <Link href="/slack_invitation" as="/slack_invitation">
-            <a>Join code-hex workspace on Slack!</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/stylish" as="/stylish">
-            <a>My Stylish Page （工事中）</a>
-          </Link>
-        </li>
-        <li>
-          <a href="https://docs.google.com/spreadsheets/d/17_8cvRg7YFruqvayDgLY22aFZm_woGt7TkWbPlhopnQ/edit#gid=0">
-            covid-19 沖縄の動向
-          </a>
-        </li>
-      </ul>
+      <div className="codehex-home">
+        <img
+          width="200px"
+          height="200px"
+          alt="codehex"
+          src="/assets/svg/codehex.svg"
+        />
+        <p>My accounts</p>
+        <ul>
+          <li>
+            <span>
+              <a href="https://okinawa.pm.org/">Okinawa.pm</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              GitHub: <a href="https://github.com/Code-Hex/">Code-Hex</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              Twitter: <a href="https://twitter.com/codehex/">@codehex</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              Hatena: <a href="http://profile.hatena.ne.jp/codehex/">codehex</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              CPAN: <a href="https://metacpan.org/author/CODEHEX/">CODEHEX</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              Blog: <a href="https://codehex.hateblo.jp/">codehex.hateblo.jp</a>
+            </span>
+          </li>
+        </ul>
+        <p>Maps</p>
+        <ul className="page-list">
+          <li>
+            <Link href="/unknown" as="/unknown">
+              <a>My Unknown List</a>
+            </Link>
+          </li>
+          <li>
+            <a href="/wikipedia.html">Random Wikipedia</a>
+          </li>
+          <li>
+            <Link href="/slack_invitation" as="/slack_invitation">
+              <a>Join code-hex workspace on Slack!</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stylish" as="/stylish">
+              <a>My Stylish Page （工事中）</a>
+            </Link>
+          </li>
+          <li>
+            <a href="https://docs.google.com/spreadsheets/d/17_8cvRg7YFruqvayDgLY22aFZm_woGt7TkWbPlhopnQ/edit#gid=0">
+              covid-19 沖縄の動向
+            </a>
+          </li>
+        </ul>
+      </div>
       <style jsx>{`
         .page-list > li {
           padding: 4px 0;
         }
 
         .marquee {
-          width: 400px;
+          width: 100%;
           overflow: hidden;
           position: relative;
         }
 
         .marquee h1 {
           white-space: nowrap;
-          padding-left: 600px;
+          padding-left: 100%;
           display: inline-block;
           animation: marquee 6s linear 0s infinite;
+        }
+
+        .codehex-home {
+          margin-left: 20px;
         }
 
         img:hover {
