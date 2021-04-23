21985d0d7f73ac9a68ab69a9227d52aca06ac7a2
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-04-23 20:03:51 +0900

    supported diff highlight

--- layouts/mdx/index.tsx
+++ layouts/mdx/index.tsx
@@ -1,5 +1,3 @@
-import 'tailwindcss/tailwind.css';
-import styles from './index.module.css';
 import { MDXProvider } from '@mdx-js/react';
 import React, { ReactNode } from 'react';
 import { MDXComponents } from '~/components/Mdx';
@@ -19,7 +17,7 @@ const Index = ({ children, frontMatter }: IndexProps) => {
   const { title } = frontMatter;
   return (
     <MDXProvider components={{ ...MDXComponents }}>
-      <div className={`${styles.footnotes} w-full flex bg-white antialiased`}>
+      <div className={`w-full flex bg-white antialiased`}>
         <div className="min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
           <div className="pb-10 border-b border-gray-200 mb-10">
             <div>
@@ -33,6 +31,7 @@ const Index = ({ children, frontMatter }: IndexProps) => {
           <div>{children}</div>
         </div>
       </div>
+      <style jsx>{``}</style>
     </MDXProvider>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -5,6 +7,46 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: '#000',
+      white: '#fff',
+
+      amber: colors.amber,
+      blue: colors.blue,
+      cyan: colors.cyan,
+      emerald: colors.emerald,
+      fuchsia: colors.fuchsia,
+      gray: colors.coolGray,
+      green: colors.green,
+      indigo: colors.indigo,
+      'light-blue': colors.lightBlue,
+      lime: colors.lime,
+      orange: {
+        ...colors.orange,
+        1000: '#4a2008',
+      },
+      pink: {
+        ...colors.pink,
+        1000: '#460d25',
+      },
+      purple: colors.purple,
+      red: colors.red,
+      rose: colors.rose,
+      teal: colors.teal,
+      violet: colors.violet,
+      yellow: colors.yellow,
+      code: {
+        punctuation: '#A1E8FF',
+        tag: '#D58FFF',
+        'attr-name': '#4BD0FB',
+        'attr-value': '#A2F679',
+        string: '#A2F679',
+        highlight: 'rgba(134, 239, 172, 0.25)',
+      },
+    },
     extend: {
       spacing: {
         '9/16': '56.25%',
@@ -14,16 +56,6 @@ module.exports = {
         12: '3rem',
         13: '3.25rem',
         14: '3.5rem',
-      },
-      colors: {
-        code: {
-          green: '#a7f3d0',
-          yellow: '#ffe484',
-          purple: '#e879f9',
-          red: '#f47067',
-          blue: '#6cb6ff',
-          white: '#acb9c6',
-        },
       },
     },
   },
