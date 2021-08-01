9448b65e57267745991cf859d257a838d4b39c3d
Code-Hex <Code-Hex@users.noreply.github.com>
2021-08-01 16:26:27 +0000

    deploy: 85c5ece6a3ed35d8ec00a4c1158258ec61636fc2

---
+++ _next/static/85c5ece6a3ed35d8ec00a4c1158258ec61636fc2/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fnote","\u002Fnote\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/e3825bca5d5570011f42d05e806989c762a1b2cc/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fnote","\u002Fnote\u002Ftag\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
