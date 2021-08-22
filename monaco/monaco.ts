7f76e40bbca4bcd2895b22825ac963f6fa028a45
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-08-22 23:10:31 +0900

    fixed language configuration

--- monaco/monaco.ts
+++ monaco/monaco.ts
@@ -1,26 +1,79 @@
-import { Monaco } from "@monaco-editor/react";
-import { MonacoLanguageLoaderResult } from "./types";
+import { Monaco } from '@monaco-editor/react';
+import { MonacoLanguageLoaderResult } from './types';
+import type * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-export const SetupEditor = (m: Monaco, markdown: MonacoLanguageLoaderResult) => {
-  m.languages.typescript.typescriptDefaults.setCompilerOptions({
-    target: m.languages.typescript.ScriptTarget.ES2016,
-    allowNonTsExtensions: true,
-    moduleResolution: m.languages.typescript.ModuleResolutionKind.NodeJs,
-    module: m.languages.typescript.ModuleKind.CommonJS,
-    noEmit: true,
-    typeRoots: ['node_modules/@types'],
-    jsx: m.languages.typescript.JsxEmit.React,
+// https://github.com/mdx-js/vscode-mdx/blob/master/language-configuration.json
+const mdxConf: monaco.languages.LanguageConfiguration = {
+  comments: {
+    blockComment: ['<!--', '-->'],
+  },
+  // symbols used as brackets
+  brackets: [
+    ['{', '}'],
+    ['[', ']'],
+    ['(', ')'],
+  ],
+  // symbols that are auto closed when typing
+  autoClosingPairs: [
+    { open: '{', close: '}' },
+    { open: '[', close: ']' },
+    { open: '(', close: ')' },
+    { open: '`', close: '`' },
+    { open: '"', close: '"', notIn: ['string'] },
+    { open: "'", close: "'", notIn: ['string'] },
+    { open: '/**', close: ' */', notIn: ['string'] },
+    { open: '<!--', close: '-->', notIn: ['string', 'comment'] },
+    { open: '<', close: '>', notIn: ['string'] },
+  ],
+  // symbols that that can be used to surround a selection
+  surroundingPairs: [
+    { open: '{', close: '}' },
+    { open: '"', close: '"' },
+    { open: "'", close: "'" },
+    { open: '<', close: '>' },
+    { open: '_', close: '_' },
+    { open: '*', close: '*' },
+    { open: '(', close: ')' },
+    { open: '[', close: ']' },
+    { open: '`', close: '`' },
+  ],
+  folding: {
+    offSide: true,
+    markers: {
+      start: new RegExp('^\\s*<!--\\s*#?region\\b.*-->'),
+      end: new RegExp('^\\s*<!--\\s*#?endregion\\b.*-->'),
+    },
+  },
+};
+
+export const SetupEditor = (
+  m: Monaco,
+  markdown: MonacoLanguageLoaderResult
+) => {
+  const compilerOptions = {
     allowJs: true,
-    reactNamespace: 'React',
-  });
+    allowSyntheticDefaultImports: true,
+    alwaysStrict: true,
+    jsx: m.languages.typescript.JsxEmit.React,
+    jsxFactory: 'React.createElement',
+  };
 
-  m.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+  m.languages.typescript.typescriptDefaults.setCompilerOptions(compilerOptions);
+  m.languages.typescript.javascriptDefaults.setCompilerOptions(compilerOptions);
+
+  m.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
     noSemanticValidation: false,
     noSyntaxValidation: false,
   });
 
+  /**
+   * Sync all the models to the worker eagerly.
+   * This enables intelliSense for all files without needing an `addExtraLib` call.
+   */
+  m.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+  m.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+
   const mdxLanguage = Object.assign({}, markdown.language);
-  const mdxConf = Object.assign({}, markdown.conf);
 
   // setup
   mdxLanguage.tokenizer.linecontent.push(
@@ -28,7 +81,7 @@ export const SetupEditor = (m: Monaco, markdown: MonacoLanguageLoaderResult) => 
     { include: 'jsxTag' }
   );
 
-  mdxLanguage.tokenizer.html = [];
+  // mdxLanguage.tokenizer.html = [];
 
   // jsx-module
   mdxLanguage.tokenizer.jsxModule = [
@@ -47,17 +100,17 @@ export const SetupEditor = (m: Monaco, markdown: MonacoLanguageLoaderResult) => 
 
   // jsx-tag
   mdxLanguage.tokenizer.jsxTag = [
-    [
-      /^(?=< *([a-zA-Z]\w*))/,
-      {
-        token: 'keyword.javascript',
-        next: '@embeddedJsxTag',
-        nextEmbedded: 'javascript',
-      },
-    ],
+    // [
+    //   /^< *([a-zA-Z]\\w*)/,
+    //   {
+    //     token: 'keyword.javascript',
+    //     next: '@embeddedJsxTag',
+    //     nextEmbedded: 'javascript',
+    //   },
+    // ],
   ];
   mdxLanguage.tokenizer.embeddedJsxTag = [
-    [/(?<=>)/, { token: '', next: '@pop', nextEmbedded: '@pop' }],
+    [/>/, { token: '', next: '@pop', nextEmbedded: '@pop' }],
   ];
 
   m.languages.register({
