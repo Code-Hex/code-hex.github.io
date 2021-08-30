a68dcb5fe59f9c5845d7dbbb541150014ddf380e
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-08-30 23:57:57 +0900

    fixed components

--- components/Note.tsx
+++ components/Note.tsx
@@ -9,7 +9,7 @@ import Prism from 'prismjs';
 
 const usePrismHighlightAll = () => {
   useEffect(() => {
-    Prism.highlightAll(true);
+    Prism.highlightAll();
   }, []);
 };
 
@@ -23,36 +23,47 @@ const Note = (props: NoteProps) => {
   const router = useRouter();
   const title = meta.title;
   return (
-    <main>
-      <article className="py-16">
-        <Head>
-          <title>{title} – codehex note</title>
-          {/* <meta name="description" content={meta.description}></meta> */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@codehex" />
-          <meta name="twitter:creator" content="@codehex" />
-          <meta name="twitter:title" content={`${title} – codehex note`} />
-          {/* <meta name="twitter:description" content={description} /> */}
-          <meta name="twitter:card" content="summary" />
-          <meta
-            name="twitter:image"
-            content={`https://codehex.dev/assets/images/twitter-card-small.jpg`}
-          />
-          <meta
-            property="og:url"
-            content={`https://codehex.dev${router.pathname}`}
-          />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content={`${title} – codehex note`} />
-          {/* <meta property="og:description" content={description} /> */}
-          <meta
-            property="og:image"
-            content={`https://codehex.dev/assets/images/twitter-card-small.jpg`}
-          />
-        </Head>
+    <>
+      <Head>
+        <title>{title} – codehex note</title>
+        {/* <meta name="description" content={meta.description}></meta> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@codehex" />
+        <meta name="twitter:creator" content="@codehex" />
+        <meta name="twitter:title" content={`${title} – codehex note`} />
+        {/* <meta name="twitter:description" content={description} /> */}
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:image"
+          content={`https://codehex.dev/assets/images/twitter-card-small.jpg`}
+        />
+        <meta
+          property="og:url"
+          content={`https://codehex.dev${router.pathname}`}
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={`${title} – codehex note`} />
+        {/* <meta property="og:description" content={description} /> */}
+        <meta
+          property="og:image"
+          content={`https://codehex.dev/assets/images/twitter-card-small.jpg`}
+        />
+      </Head>
+      <main>
         <NoteContent title={title}>{children}</NoteContent>
-      </article>
-    </main>
+      </main>
+      <footer className="min-w-0 flex-auto px-8 sm:px-10 xl:px-12 pt-10 pb-24 lg:pb-16">
+        <div className="text-sm font-medium leading-5 divide-y divide-gray-200 xl:col-start-1 xl:row-start-2">
+          <div className="pt-8">
+            <Link href="/note">
+              <a className="text-teal-500 hover:text-teal-600">
+                ← Back to the note
+              </a>
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
@@ -65,7 +76,7 @@ export const NoteContent = ({ title, children }: NoteContentProps) => {
   usePrismHighlightAll();
   return (
     <div className={`w-full flex bg-white antialiased`}>
-      <div className="min-w-0 flex-auto px-8 sm:px-10 xl:px-12 pt-10 pb-24 lg:pb-16">
+      <div className="flex-auto px-8 sm:px-10 xl:px-12 pt-10 pb-24 lg:pb-16">
         <div className="pb-2 border-b border-gray-200 mb-10">
           <h1 className="inline-block text-3xl font-bold text-gray-900 tracking-tight">
             {title}
@@ -76,15 +87,6 @@ export const NoteContent = ({ title, children }: NoteContentProps) => {
             {children}
           </MDXProvider>
         </article>
-        <footer className="text-sm font-medium leading-5 divide-y divide-gray-200 xl:col-start-1 xl:row-start-2">
-          <div className="pt-8">
-            <Link href="/note">
-              <a className="text-teal-500 hover:text-teal-600">
-                ← Back to the note
-              </a>
-            </Link>
-          </div>
-        </footer>
       </div>
     </div>
   );
