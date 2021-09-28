81cfe57f590d3b633c380b1008c930ec356c8114
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-09-28 22:55:18 +0900

    fixed tailwind effect

--- src/layouts/NoteItems.tsx
+++ src/layouts/NoteItems.tsx
@@ -19,13 +19,12 @@ export const NoteItems = ({
   useEffect(() => {
     Prism.highlightAll();
   }, []);
-
   return (
     <div className="w-full mx-auto antialiased max-w-3xl xl:max-w-5xl">
       <div className="mx-8 sm:mx-10 md:mx-12 py-10">
         <main className="divide-y divide-gray-200">
-          <div className="text-left py-4">
-            <h1 className="tracking-tight font-extrabold text-gray-900 text-4xl">
+          <div className="py-4">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900">
               {title}
             </h1>
             {children}
