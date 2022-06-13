a2904992aca354d2ff7dbd7208c801de1169f867
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-06-13 20:43:06 +0900

    added adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,17 @@ import '../styles/mdx.css';
 import type { AppProps } from 'next/app';
 import { gaId, usePageView } from '~/lib/gtag';
 import { GoogleAnalytics } from '~/components/GoogleAnalytics';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView();
   return (
     <>
       <GoogleAnalytics gaId={gaId} />
+      <Script
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8097329174824434"
+      />
       <Component {...pageProps} />
     </>
   );
