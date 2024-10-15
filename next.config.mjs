a765594fb556c6027c6ff49e0972e48862aed5ff
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-10-15 18:43:06 +0900

    removed export

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
  * @type {import("next").NextConfig}
  */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
   pageExtensions: ['ts', 'tsx'],
   // https://nextjs.org/docs/basic-features/image-optimization
